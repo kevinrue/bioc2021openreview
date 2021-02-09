7c3ac238f4a46ae809e83fb9ff02e21a41f7833a
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2021-02-09 14:11:54 +0000

    export configuration to separate file; set up renv; update gitignore

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+username = os.environ["OPENREVIEW_USERNAME"]
+password = os.environ["OPENREVIEW_PASSWORD"]
+
+conference = "bioconductor.org/BioC/2021/Conference"
--- get_submissions.py
+++ get_submissions.py
@@ -1,15 +1,13 @@
 import openreview
-import os
 
-username = os.environ["OPENREVIEW_USERNAME"]
-password = os.environ["OPENREVIEW_PASSWORD"]
-
+import config
 
 c = openreview.Client(
-    baseurl="https://api.openreview.net", username=username, password=password
+    baseurl="https://api.openreview.net", username=config.username, password=config.password
 )
 
-notes = c.get_notes(invitation="bioconductor.org/EuroBioC/2020/Conference/-/Submission")
+notes = c.get_notes(invitation=config.conference + "/-/Submission")
 
-len(notes)
-notes[0]
+print(len(notes))
+print(notes[0])
+
